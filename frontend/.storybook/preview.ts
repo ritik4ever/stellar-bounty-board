c2d7ad83d4a3c9de759d9aa78ef1744c2ddcd116
taiwoabdulsamad1-coder <taiwoabdulsamad1@gmail.com>
2026-08-28 12:56:42 +0100

    fix: [CHORE] Add visual regression testing setup (Chromatic or Pe (#851)

--- frontend/.storybook/preview.ts
+++ frontend/.storybook/preview.ts
@@ -1,7 +1,9 @@
 import "../src/index.css";
+import { withChromatic } from "@chromatic-com/storybook";
 import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
+  decorators: [withChromatic],
   parameters: {
     controls: { matchers: { color: /(background|color)$/i, date: /Date$/i } },
   },
